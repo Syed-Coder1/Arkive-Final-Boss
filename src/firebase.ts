de25e55f58f705d2a01e42a7268fd1783609e010
Syed-Coder1 <syedm.ali.8818@gmail.com>
2025-08-08 13:01:59 +0500

    Updated firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,13 @@
+// firebase.ts
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDIo7q8OuI1P63q9t9E1s-ENQjBdCd37nI",
   authDomain: "arkive-da661.firebaseapp.com",
-  databaseURL: "https://arkive-da661-default-rtdb.firebaseio.com", // ✅ FIXED HERE
+  databaseURL: "https://arkive-da661-default-rtdb.firebaseio.com", // ✅ Correct RTDB URL
   projectId: "arkive-da661",
-  storageBucket: "arkive-da661.firebasestorage.app",
+  storageBucket: "arkive-da661.appspot.com", // ✅ Fixed storage bucket
   messagingSenderId: "416097604327",
   appId: "1:416097604327:web:198600d582bd82aeee8842"
 };
